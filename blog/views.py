00f147a86dd3972e7e8d6f85e2bb21bab36931cf
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2018-06-30 11:07:05 +0800

    Link to blog post

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 # Create your views here.
@@ -9,3 +9,8 @@ def post_list(request):
     #     published_date__lte=timezone.now()).order_by('published_date')
     posts = Post.objects.order_by('published_date')
     return render(request, 'post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post': post})
