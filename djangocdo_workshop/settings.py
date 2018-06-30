4fb5d4cd5f08a2a5d3b5b368a3221af08b72653f
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2018-06-30 18:46:53 +0800

    Fixes on database

--- djangocdo_workshop/settings.py
+++ djangocdo_workshop/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import dj-database-url  # add this to the top of your file
+# import dj-database-url  # add this to the top of your file
 
 db_from_env = dj_database_url.config()
 DATABASES[‘default’].update(db_from_env)
