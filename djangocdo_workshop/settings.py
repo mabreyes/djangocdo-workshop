7906478bbaf32ec06fb6305833e5d826a127ec40
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2018-06-30 18:42:51 +0800

    Fix heroku failure

--- djangocdo_workshop/settings.py
+++ djangocdo_workshop/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj-database-url  # add this to the top of your file
+
+db_from_env = dj_database_url.config()
+DATABASES[‘default’].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
