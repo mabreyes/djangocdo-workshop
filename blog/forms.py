b6de29098c3bb14c3f2360dd01afa7355b0e7587
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2018-06-30 15:56:17 +0800

    Implement add new blog entry and edit entry

---
+++ blog/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text')
+
+    # Source: https://gist.github.com/rochacon/1514222
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        for key, field in self.fields.items():
+            if isinstance(field.widget, forms.TextInput) or \
+                    isinstance(field.widget, forms.Textarea) or \
+                    isinstance(field.widget, forms.DateInput) or \
+                    isinstance(field.widget, forms.DateTimeInput) or \
+                    isinstance(field.widget, forms.TimeInput):
+                field.widget.attrs.update({'placeholder': field.label})
